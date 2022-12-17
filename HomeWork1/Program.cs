1a87046358400295ae158eabcde3f65ed671a527
Elena <atma12@yandex.ru>
2022-12-17 21:31:25 +0300

    Task1

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2
+/*Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+{
+    Console.Write("max = num1; min = num2");
+}
+else if (num1 < num2)
+{
+    Console.Write("max = num2; min = num1");
+}
+else 
+{
+    Console.Write("num1 = num2");
+}*/
