442deb32990f3a06cf6a3df62d6fe3fb36f051c9
Elena <atma12@yandex.ru>
2022-12-17 21:42:12 +0300

    Task2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -16,3 +16,21 @@ else
 {
     Console.Write("num1 = num2");
 }*/
+
+//Задача 4
+/*Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.Write(max);
+*/
+
